4123d62d63a18959fef2b6d5ef521bf553b0f027
bh <gungthar@gmail.com>
2018-09-04 08:25:57 -0500

    added conf

---
+++ user/keyboards/ortho_6x14/config.h
@@ -0,0 +1,7 @@
+#include "config_common.h"
+
+#define MATRIX_ROWS 6
+#define MATRIX_COLS 14
+
+#define MATRIX_ROW_PINS { 19, 13, 14, 15, 16, 17}
+#define MATRIX_COL_PINS { 18, 8, 20, 7, 21, 6, 22, 5, 23, 4, 3, 2, 1, 0 }
